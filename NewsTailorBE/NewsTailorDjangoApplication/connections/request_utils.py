65d9bed369486cd5883d01389dddc7c86e2e1581
Duarte Cardoso <caldasdcardoso@gmail.com>
2024-10-30 16:23:11 +0000

    Fixing translation and adding unit tests to the solution

--- NewsTailorBE/NewsTailorDjangoApplication/connections/request_utils.py
+++ NewsTailorBE/NewsTailorDjangoApplication/connections/request_utils.py
@@ -1,7 +1,8 @@
 English_language_prefix = "en"
+German_language_prefix = "de"
 
 
 def translate_user_preferred_language(request_language: str) -> str:
-    if request_language == "English":
-        return English_language_prefix
+    if request_language == "German":
+        return German_language_prefix
     return English_language_prefix
