d803721f2137392c65a3f15994b5e5d22abf5c14
Duarte Cardoso <caldasdcardoso@gmail.com>
2024-10-30 14:41:59 +0000

    Creating a generic request to guardian and news_api

--- NewsTailorBE/NewsTailorDjangoApplication/api-connections/news-api.py
+++ NewsTailorBE/NewsTailorDjangoApplication/api-connections/news-api.py
@@ -3,14 +3,15 @@ import requests
 from dotenv import load_dotenv
 from request_utils import obtain_user_preferred_language
 
-NewsAPIUrl = "https://newsapi.org/v2/top-headlines/sources"
 load_dotenv()
+NewsAPIUrl = "https://newsapi.org/v2/top-headlines/sources"
 news_api_key = os.getenv("NEWS_API_KEY")
+guardian_api_key = os.getenv("GUARDIAN_API_KEY")
 
 
-def technology_request_to_news_api() -> requests:
+def general_request_from_news_api(category: str) -> requests:
     params = {
-        "category": "technology",
+        "category": category,
         "apiKey": news_api_key,
         "language": obtain_user_preferred_language(),
     }
@@ -21,10 +22,14 @@ def technology_request_to_news_api() -> requests:
         print(response.json())
     else:
         print(f"Request failed with status code {response.status_code}")
-
-    print(response)
-
     return response
 
 
-technology_request_to_news_api()
+def general_request_from_guardian_api(category: str) -> requests:
+    url = "https://content.guardianapis.com/search"
+    params = {"api-key": guardian_api_key, "section": category, "page-size": 10}
+    response = requests.get(url, params=params)
+    data = response.json()
+    print(data)
+
+    return data
