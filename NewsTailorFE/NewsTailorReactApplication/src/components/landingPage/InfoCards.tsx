d1de78000069436f055fadf5c87c88961d6c8a85
Maria Laranjeira <up202004453@up.pt>
2024-11-06 10:44:29 +0000

    Replace info cards icons

--- NewsTailorFE/NewsTailorReactApplication/src/components/landingPage/InfoCards.tsx
+++ NewsTailorFE/NewsTailorReactApplication/src/components/landingPage/InfoCards.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import '../../styles/landingPage/InfoCards.css';
+import { IoVolumeMute, IoReader, IoEarth, IoStopwatch } from "react-icons/io5";
+
 
 interface InfoCard {
   text: string;
-  icon: string;
+  icon: React.ReactNode;
 }
 
 const infoCards: InfoCard[] = [
-  { icon: "📰", text: "Get the news that matters to you. Our AI curates articles from across the web based on your selected interests." },
-  { icon: "⏱", text: "Short on time? Let us know how much time you have, and NewsTailor will adjust the news length just for you." },
-  { icon: "🌐", text: "Stay up-to-date with real-time news summaries, keeping you informed on the latest in your favorite topics." },
-  { icon: "🔇", text: "Cut through the noise. NewsTailor filters out irrelevant content, delivering only what you need to know." },
+  { icon: <IoReader />, text: "Get the news that matters to you. Our AI curates articles from across the web based on your selected interests." },
+  { icon: <IoStopwatch />, text: "Short on time? Let us know how much time you have, and NewsTailor will adjust the news length just for you." },
+  { icon: <IoEarth />, text: "Stay up-to-date with real-time news summaries, keeping you informed on the latest in your favorite topics." },
+  { icon: <IoVolumeMute />, text: "Cut through the noise. NewsTailor filters out irrelevant content, delivering only what you need to know." },
 ];
 
 const InfoCards: React.FC = () => {
