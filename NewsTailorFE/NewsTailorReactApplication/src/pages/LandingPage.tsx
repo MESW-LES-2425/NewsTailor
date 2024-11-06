1433fab853c969493a2d9e92709109bb475ac335
Maria Laranjeira <up202004453@up.pt>
2024-11-06 02:16:09 +0000

    Add stylesheet for landing page

--- NewsTailorFE/NewsTailorReactApplication/src/pages/LandingPage.tsx
+++ NewsTailorFE/NewsTailorReactApplication/src/pages/LandingPage.tsx
@@ -1,15 +1,17 @@
 import AuthButton from '../components/landingPage/AuthButton';
 import Header from '../components/landingPage/Header';
 import InfoCards from '../components/landingPage/InfoCards';
+import '../styles/LandingPage.css';
+
 
 function LandingPage() {
-    return (
-      <div>
-        <Header />
-        <AuthButton />
-        <InfoCards />
-      </div>
-    );
+  return (
+    <div className="landing-page-container">
+      <Header />
+      <AuthButton />
+      <InfoCards />
+    </div>
+  );
 }
 
 export default LandingPage
