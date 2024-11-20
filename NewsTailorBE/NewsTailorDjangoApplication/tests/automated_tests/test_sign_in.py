c839d2ed934e89650db3a1a1eb20c95ebf8b0b0a
Duarte Cardoso <caldasdcardoso@gmail.com>
2024-11-20 15:55:39 +0000

    Adding the new chrome unit test

--- NewsTailorBE/NewsTailorDjangoApplication/tests/automated_tests/test_sign_in.py
+++ NewsTailorBE/NewsTailorDjangoApplication/tests/automated_tests/test_sign_in.py
@@ -3,11 +3,11 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 class TestSignin:
-  def setup_method(self):
-    self.driver = webdriver.Firefox()
+  def setup_method(self, method):
+    self.driver = webdriver.Chrome()
     self.vars = {}
   
-  def teardown_method(self):
+  def teardown_method(self, method):
     self.driver.quit()
   
   def test_signin(self):
@@ -15,24 +15,24 @@ class TestSignin:
     # Step # | name | target | value
     # 1 | open | / | 
     self.driver.get("http://localhost:5173/")
-    # 2 | setWindowSize | 1472x911 | 
-    self.driver.set_window_size(1472, 911)
+    # 2 | setWindowSize | 1512x823 | 
+    self.driver.set_window_size(1512, 823)
     # 3 | click | css=.auth-button | 
     self.driver.find_element(By.CSS_SELECTOR, ".auth-button").click()
-    # 4 | click | css=.sign-in-form > .signin-signup-input-field:nth-child(2) | 
-    self.driver.find_element(By.CSS_SELECTOR, ".sign-in-form > .signin-signup-input-field:nth-child(2)").click()
-    # 5 | click | css=.sign-in-form > .signin-signup-input-field:nth-child(2) | 
-    self.driver.find_element(By.CSS_SELECTOR, ".sign-in-form > .signin-signup-input-field:nth-child(2)").click()
-    # 6 | click | css=.sign-in-form > .signin-signup-input-field:nth-child(2) > input | 
-    self.driver.find_element(By.CSS_SELECTOR, ".sign-in-form > .signin-signup-input-field:nth-child(2) > input").click()
-    # 7 | type | css=.sign-in-form > .signin-signup-input-field:nth-child(2) > input | user@gmail.com
-    self.driver.find_element(By.CSS_SELECTOR, ".sign-in-form > .signin-signup-input-field:nth-child(2) > input").send_keys("user@gmail.com")
-    # 8 | click | css=.sign-in-form > .signin-signup-input-field:nth-child(3) | 
-    self.driver.find_element(By.CSS_SELECTOR, ".sign-in-form > .signin-signup-input-field:nth-child(3)").click()
-    # 9 | click | name=password | 
-    self.driver.find_element(By.NAME, "password").click()
-    # 10 | type | name=password | 12345Aa!
+    # 4 | type | name=username | caldasdcardoso@gmail.com
+    self.driver.find_element(By.NAME, "username").send_keys("caldasdcardoso@gmail.com")
+    # 5 | type | name=password1 | 12345Aa!
+    self.driver.find_element(By.NAME, "password1").send_keys("12345Aa!")
+    # 6 | type | css=.sign-in-form > .signin-signup-input-field:nth-child(2) > input | caldasdcardoso@gmail.com
+    self.driver.find_element(By.CSS_SELECTOR, ".sign-in-form > .signin-signup-input-field:nth-child(2) > input").send_keys("caldasdcardoso@gmail.com")
+    # 7 | type | name=password | 12345Aa!
     self.driver.find_element(By.NAME, "password").send_keys("12345Aa!")
+    # 8 | click | css=.sign-in-form > .signin-signup-input-field:nth-child(2) | 
+    self.driver.find_element(By.CSS_SELECTOR, ".sign-in-form > .signin-signup-input-field:nth-child(2)").click()
+    # 9 | type | css=.sign-in-form > .signin-signup-input-field:nth-child(2) > input | user@gmail.com
+    self.driver.find_element(By.CSS_SELECTOR, ".sign-in-form > .signin-signup-input-field:nth-child(2) > input").send_keys("user@gmail.com")
+    # 10 | click | css=.sign-in-form | 
+    self.driver.find_element(By.CSS_SELECTOR, ".sign-in-form").click()
     # 11 | click | css=.solid | 
     self.driver.find_element(By.CSS_SELECTOR, ".solid").click()
   
