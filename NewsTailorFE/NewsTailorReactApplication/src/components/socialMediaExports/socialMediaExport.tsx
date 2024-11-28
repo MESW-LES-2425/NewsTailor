dadf5c08e4da505119c9baf1bcc218bae29a39b0
Duarte Cardoso <caldasdcardoso@gmail.com>
2024-11-28 20:39:01 +0000

    Styling changes and behavior corrections

--- NewsTailorFE/NewsTailorReactApplication/src/components/socialMediaExports/socialMediaExport.tsx
+++ NewsTailorFE/NewsTailorReactApplication/src/components/socialMediaExports/socialMediaExport.tsx
@@ -1,21 +1,22 @@
 import {    EmailShareButton,WhatsappShareButton,LinkedinShareButton,TwitterShareButton } from "react-share";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
-import { faWhatsapp, faLinkedin, faTwitter } from "@fortawesome/free-brands-svg-icons"; 
+import { faWhatsapp, faLinkedin, faXTwitter } from "@fortawesome/free-brands-svg-icons"; 
 
 import "./socialMediaStyling.css";
 
 const ShareButtons = ({ initialContent = "" }) => {
     const articleText = initialContent;
-    const url = "http://google.com";
+    // TODO change this to our domain URL as soon as we have one
+    const url = "http://localhost:5173";
 
     return (
         <div>
             <div className="icon-wrapper" title="Share via Email">
                 <EmailShareButton
                     url={url}
-                    subject="Check out this article from NewsTailor!"
-                    body={`Hi, I found this article interesting: ${articleText}\n\nCheck it out here: ${url}`}
+                    subject="Check out this Newspaper I just generated with NewsTailor!"
+                    body={`${articleText}\n\nCheck it out here: ${url}`}
                 >
                     <FontAwesomeIcon icon={faEnvelope} size="lg" />
                 </EmailShareButton>
@@ -30,23 +31,22 @@ const ShareButtons = ({ initialContent = "" }) => {
             <div className="icon-wrapper" title="Share via LinkedIn">
                 <LinkedinShareButton
                     url={url}
-                    title="Check this out!"
-                    summary="Great article!"
+                    title="Check out NewsTailor!"
+                    summary="I just generated a newspaper with NewsTailor!"
                     source="NewsTailor"
-                    
                 >
                     <FontAwesomeIcon icon={faLinkedin} size="lg" />
                 </LinkedinShareButton>
             </div>
 
-            <div className="icon-wrapper" title="Share via Twitter">
+            <div className="icon-wrapper" title="Share via X">
                 <TwitterShareButton
                     url={url}
-                    title="Check out this article!"
+                    title="I just generated a newspaper with NewsTailor!"
                     via="NewsTailor"
-                    hashtags={["news", "trending"]}
+                    hashtags={["news", "ai", "newspaper"]}
                 >
-                    <FontAwesomeIcon icon={faTwitter} size="lg" />
+                    <FontAwesomeIcon icon={faXTwitter} size="lg" />
                 </TwitterShareButton>
             </div>
         </div>
