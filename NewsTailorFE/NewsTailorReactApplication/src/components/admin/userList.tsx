26431bebe0d8f0e0bf380bfa1ee7d2d3780b13c5
Pedro Magalhães <up202108756@up.pt>
2024-12-11 22:01:11 +0000

    Admin page
    First iteration of the full admin page with the user ability to ban other users and the frontend styling. this page is still accessible by all users to facilitate development

--- NewsTailorFE/NewsTailorReactApplication/src/components/admin/userList.tsx
+++ NewsTailorFE/NewsTailorReactApplication/src/components/admin/userList.tsx
@@ -1,28 +1,35 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import api from "../../api.ts";
+import "./admin.css";
+import {faBan, faCheck} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 
 
 const UserList = () => {
-const [users, setUsers] = useState<{user_id: number, username: string; is_banned: boolean }[]>([]);
+    const [users, setUsers] = useState<{user_id: number, username: string; is_banned: boolean }[]>([]);
+    const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-      const fetchUsers = async () => {
-          try {
-              const response = await api.get(`/api/users/`);
-              setUsers(Array.isArray(response.data) ? response.data : []);
-              console.log(response.data);
-          } catch (error) {
-              if (axios.isAxiosError(error) && error.response) {
-                  console.error(error.response.data);
-              } else {
-                  console.error("Error fetching users", error);
-              }
-          }
-      };
+    const fetchUsers = async () => {
+        setLoading(true);
+        try {
+            const response = await api.get(`/api/users/`);
+            setUsers(Array.isArray(response.data) ? response.data : []);
+        } catch (error) {
+            if (axios.isAxiosError(error) && error.response) {
+                console.error(error.response.data);
+            } else {
+                console.error("Error fetching users", error);
+            }
+        } finally {
+            setLoading(false);
+        }
+    };
 
-      fetchUsers()
-  }, []);
+    fetchUsers();
+}, []);
 
   const banUser = (user_id: number) => async () => {
     try {
@@ -45,24 +52,31 @@ const [users, setUsers] = useState<{user_id: number, username: string; is_banned
     }
   }
 
-  return (
+return (
     <div>
-      <h1>Users</h1>
-      <ul>
-        {users.map(user => (
-            <li key={user.user_id}>{user.username} - {user.is_banned ? "Banned" : "Active"}
-                <button onClick={banUser(user.user_id)}>
-                    {user.is_banned ?
-                        "Unban"
-                        :
-                        "Ban"
-                    }
-                </button>
-            </li>
-        ))}
-      </ul>
+        <h1>Users</h1>
+        {loading ? (
+            <div className="spinner">
+                <FontAwesomeIcon icon={faSpinner} spin size="3x" />
+            </div>
+        ) : (
+            <ul>
+                {users.map(user => (
+                    <li key={user.user_id}>
+                        <p className={user.is_banned ? "banned" : ""}>{user.username}</p>
+                        <button onClick={banUser(user.user_id)}>
+                            {user.is_banned ?
+                                <FontAwesomeIcon icon={faCheck} size="lg" />
+                                :
+                                <FontAwesomeIcon icon={faBan} size="lg" />
+                            }
+                        </button>
+                    </li>
+                ))}
+            </ul>
+        )}
     </div>
-  );
-};
+);
+}
 
 export default UserList;
